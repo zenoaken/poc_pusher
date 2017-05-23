b086765e95b3b9463c110a74966c30f99c76a311
Luiz Carvalho <lcorreadecarvalhojr@zendesk.com>
2017-05-23 14:15:05 +0100

    refactor

--- server.py
+++ server.py
@@ -17,15 +17,45 @@ pusher_client = pusher.Pusher(
 )
 
 
-users_status = {}
+# users_status = {}
+users_on_channel = {}
+channels_per_user = {}
+resources_per_user = {}
+
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Agent Presence Test Server"
 
 
-def get_user_status(username):
-    return users_status.get(username, {"name": username, "status": "unknown", "time_ms": 0, "resources": defaultdict(dict)})
+def get_users_on_channel(channel):
+    if not users_on_channel.get(channel):
+        users_on_channel[channel] = {}
+    return users_on_channel[channel]
+
+
+def get_user_on_channel(channel, username):
+    if not get_users_on_channel(channel).get(username):
+        get_users_on_channel(channel)[username] = {}
+    return get_users_on_channel(channel)[username]
+
+
+def get_channels_per_user(username):
+    if not channels_per_user.get(username):
+        channels_per_user[username] = {}
+    return channels_per_user[username]
+
+
+def get_channel_per_user(username, channel):
+    if not get_channels_per_user(username).get(channel):
+        get_channels_per_user(username)[channel] = {}
+    return get_channels_per_user(username)[channel]
+
+
+def get_resources_per_user(username):
+    if not resources_per_user.get(username):
+        resources_per_user[username] = {}
+    return resources_per_user[username]
 
 
 @app.route("/api/pusher/auth", methods=['POST'])
@@ -47,8 +77,6 @@ def pusher_auth():
 
 @app.route("/api/hooks/presence", methods=['POST'])
 def presence_webhook():
-    global users_status
-
     webhook = pusher_client.validate_webhook(
         key=request.headers.get('X-Pusher-Key'),
         signature=request.headers.get('X-Pusher-Signature'),
@@ -59,45 +87,48 @@ def presence_webhook():
     for event in webhook["events"]:
         channel = event["channel"]
         user = event["user_id"]
-        current_user_status = get_user_status(user)
 
-        if channel.startswith("presence-user-"):
+        if channel.startswith("presence-user-on-product-"):
+            product = channel.split("-")[-2]
+
+            current_status = get_user_on_channel(product, user)
+
             # continue if we already have a most recent information
-            if current_user_status["time_ms"] > webhook_time_ms:
+            if current_status and current_status["time_ms"] > webhook_time_ms:
                 continue
 
-            status = "online" if event["name"] == "member_added" else "offline" if event[
+            status = "available" if event["name"] == "member_added" else "offline" if event[
                                                                                        "name"] == "member_removed" else None
 
             if status:
-                if status == "online":
-                    current_user_status["status"] = status
-                    current_user_status["time_ms"] = webhook_time_ms
-                    users_status[user] = current_user_status
+                if status == "available":
+                    current_status["status"] = status
+                    current_status["time_ms"] = webhook_time_ms
+                    get_users_on_channel(product)[user] = current_status
+                    get_channels_per_user(user)[product] = current_status
                 elif status == "offline":
-                    users_status.pop(user, None)
-                pusher_client.trigger('private-user-status-changed', 'client-status-changed',
+                    get_users_on_channel(product).pop(user)
+                    get_channels_per_user(user).pop(product)
+                pusher_client.trigger("private-user-status-changed-on-%s" % product, 'client-status-changed',
                                       {'user': user, 'status': status})
         elif channel.startswith("presence-users-on-resource-"):
             resource_type, resource_id = channel.split("-")[4:6]
             resource_key = "%s_%s" % (resource_type, resource_id)
 
             if event["name"] == "member_added":
-                current_user_status["resources"][resource_key] = {
+                get_resources_per_user(user)[resource_key] = {
                     "type": resource_type,
                     "id": resource_id,
                     "action": "viewing"
                 }
             elif event["name"] == "member_removed":
-                current_user_status["resources"].pop(resource_key, None)
+                get_resources_per_user(user).pop(resource_key, None)
 
     return "ok"
 
 
 @app.route("/api/hooks/client-events", methods=['POST'])
 def client_events_webhook():
-    global users_status
-
     webhook = pusher_client.validate_webhook(
         key=request.headers.get('X-Pusher-Key'),
         signature=request.headers.get('X-Pusher-Signature'),
@@ -110,34 +141,56 @@ def client_events_webhook():
 
         event_data = json.loads(event["data"])
         user = event_data["user"]
-        current_user_status = users_status.get(user, {"status": "unknown", "time_ms": 0})
 
-        if channel == "private-user-status-changed":
+        if channel.startswith("private-user-status-changed-on-"):
+            product = channel.split("-")[-1]
             status = event_data["status"]
 
+            current_status = get_user_on_channel(product, user)
+
             # continue if we already have a most recent information
-            if current_user_status["time_ms"] > webhook_time_ms:
+            if not current_status or current_status["time_ms"] > webhook_time_ms:
                 continue
 
-            users_status[user] = {"status": status, "time_ms": webhook_time_ms}
+            current_status["status"] = status
+            current_status["time_ms"] = webhook_time_ms
         elif channel.startswith("presence-users-on-resource-"):
             resource_type, resource_id = channel.split("-")[4:6]
             resource_key = "%s_%s" % (resource_type, resource_id)
 
-            if current_user_status["resources"][resource_key]:
-                current_user_status["resources"][resource_key]["action"] = event["event"].split("-")[-1]
+            if get_resources_per_user(user)[resource_key]:
+                get_resources_per_user(user)[resource_key]["action"] = event["event"].split("-")[-1]
 
     return "ok"
 
 
 @app.route("/api/users")
 def users():
-    return jsonify(users_status.values())
+    return jsonify(channels_per_user.values())
+
+
+@app.route("/api/resources")
+def resources_in_use():
+    return jsonify(resources_per_user.keys())
+
+
+@app.route("/api/resource/<type>/<id>/presence")
+def users_in_resource():
+    raise NotImplementedError
 
 
 @app.route("/api/user/<username>/resources")
-def user_resources(username):
-    return jsonify(users_status.get(username, {}).get("resources", {}).values())
+def resources_per_user(username):
+    resources = filter(
+        lambda resource: resource["type"] == request.args.get('type'),
+        get_resources_per_user(username).values()
+    ) if request.args.get('type') else get_resources_per_user(username).values()
+    return jsonify(resources)
+
+
+@app.route("/api/channel/<channel>/users")
+def user_in_channel(channel):
+    return jsonify(get_users_on_channel(channel).values())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=environ.get("PORT", 5000))
